907e9c28a83a0bdf2959404b771da569276d1e5a
ItsMeIshan <65131563+ItsMeIshan@users.noreply.github.com>
2021-07-25 13:56:26 +0530

    fix: edTech word list increased(#14)

--- Commands/restrictedWords.js
+++ Commands/restrictedWords.js
@@ -24,18 +24,31 @@ const words = [
     'cfc',
     'code for cause',
     'anuj',
-    'ganga'
+    'ganga',
+    'codeforcause',
+    'pepcoding',
+    'pep coding',
+    'codedamn',
+    'codingblocks',
+    'coding blocks',
+    'byjus',
+    `byju's`,
+    'white hat jr',
+    'white hat junior',
+    'whitehatjunior',
+    'geeksforgeeks',
+    'geeks for geeks',
+    'gfg'
+
 ]
 
 module.exports = (client, callback) => {
     client.on('message', message => {
-
         for (var i=0; i < words.length; i++) {
             if(message.content.toLowerCase().includes(words[i])) {
                 message.reply('Please do not talk about other edTech startups here 🚫. If you think I did a mistake dont worry I am still under development, tag Community Manager and report this 🏷️. ');
                 message.author.send(warnEmbed);
             }
         }
-
     });
 };
