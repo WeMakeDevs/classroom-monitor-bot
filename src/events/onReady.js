3be8e3acf2565b6726881efd20809a773fa73844
Dhans <sgdhans@gmail.com>
2021-12-29 11:12:21 +0530

    feat: rest works finnaly

--- src/events/onReady.js
+++ src/events/onReady.js
@@ -3,6 +3,7 @@ const { Routes } = require('discord-api-types/v9');
 const { CommandList } = require('../Commands/_COMMANDS');
 const { botErrorHandler } = require('../utils/botErrorHandler');
 const { botLogHandler } = require('../utils/botLogHandler');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 const onReady = async (client) => {
 	try {
@@ -11,13 +12,29 @@ const onReady = async (client) => {
 		const commandData = [];
 
 		CommandList.forEach((command) => commandData.push(command.data.toJSON()));
-		await rest.post(
-			Routes.applicationGuildCommand(
-				client.user?.id || 'missing token',
-				process.env.GUILD_ID
-			),
-			{ body: commandData }
-		);
+		//sample command data
+		// const commands = [
+		// 	new SlashCommandBuilder()
+		// 		.setName('ping')
+		// 		.setDescription('Replies with pong!'),
+		// 	new SlashCommandBuilder()
+		// 		.setName('server')
+		// 		.setDescription('Replies with server info!'),
+		// 	new SlashCommandBuilder()
+		// 		.setName('user')
+		// 		.setDescription('Replies with user info!'),
+		// ].map((command) => command.toJSON());
+
+		await rest
+			.put(
+				Routes.applicationGuildCommands(
+					client.user?.id || 'missing token',
+					process.env.GUILD_ID
+				),
+				{ body: commandData }
+			)
+			.then(() => console.log('Successfully registered application commands.'))
+			.catch(console.error);
 
 		botLogHandler.log('debug', 'Bot has connected to Discord!');
 	} catch (error) {
