4acb5c3c5456090db5e699fe5205f20605bc31c6
Dhans <sgdhans@gmail.com>
2021-12-29 11:51:19 +0530

    feat: meme slashcommand

--- src/Commands/_COMMANDS.js
+++ src/Commands/_COMMANDS.js
@@ -2,7 +2,8 @@ const { DSA } = require('./DSA');
 const { Help } = require('./help');
 const { Hey } = require('./hey');
 const { Links } = require('./links');
+const { Meme } = require('./meme');
 
-const CommandList = [DSA, Help, Hey, Links];
+const CommandList = [DSA, Help, Hey, Links, Meme];
 
 module.exports = { CommandList };
--- src/Commands/meme.js
+++ src/Commands/meme.js
@@ -1,23 +1,27 @@
 const { meme } = require('memejs');
 const { botErrorHandler } = require('../utils/botErrorHandler');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
-try {
-	module.exports = {
-		name: 'meme',
-		desc: 'send a programmer humor meme',
-		async execute(message, args, Discord) {
-			try {
-				let memeResponse = await meme('programmerhumor');
+const Meme = {
+	data: new SlashCommandBuilder()
+		.setName('meme')
+		.setDescription('send a programmer humor meme'),
 
-				let memeEmbed = new Discord.MessageEmbed().setImage(memeResponse.url);
-				if (memeResponse.title) memeEmbed.setTitle(memeResponse.title);
-				message.channel.send(memeEmbed);
-			} catch (err) {
-				console.log(err.message);
-				message.channel.send('I had an error :(');
+	run: async (interaction) => {
+		try {
+			await interaction.deferReply();
+			let memeResponse = await meme('programmerhumor');
+			const memeEmbed = new MessageEmbed();
+
+			if (memeResponse.title) {
+				memeEmbed.setTitle(memeResponse.title);
 			}
-		},
-	};
-} catch (error) {
-	botErrorHandler('meme command', error);
-}
+
+			await interaction.editReply({ embeds: [memeEmbed] });
+		} catch (error) {
+			botErrorHandler('Help command', error);
+		}
+	},
+};
+module.exports = { Meme };
